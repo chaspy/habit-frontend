b19d9fbf2bca00f3d4e121ea1981de11849bdcfd
kondo takeshi <chaspy@users.noreply.github.com>
2022-09-04 17:21:22 +0900

    hello apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { useEffect } from 'react'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+})
 
 function App() {
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          query {
+            habit(id: "2") {
+              id
+              name
+              records {
+                id
+                date
+              }
+            }
+          }
+        `,
+      })
+      .then((result) => console.log(result))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Apollo Client</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
